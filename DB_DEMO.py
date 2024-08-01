f37224b41055cb1055dd0c2aa18db819a5498034
jayhong329 <smoja329@gmail.com>
2024-08-01 16:36:23 +0800

    資料讀取select

--- DB_DEMO.py
+++ DB_DEMO.py
@@ -4,8 +4,10 @@ class Category():
     def __init__():
         pass
 
-    # 建立連線
     def categories():
+        # 其他程式 如爬蟲程式
+
+        # Step1 建立連線
         connection = MySQLdb.connect(
             host = "localhost",
             database = "sakila",
@@ -15,4 +17,17 @@ class Category():
         if not connection:
             return None
         
-        return connection
+        # Step2 SQL語法
+        sql = "select * from category"
+
+        # Step3 建立cursor物件執行SQL語法
+        with connection:
+            with connection.cursor() as cursor:
+                try:
+                    cursor.execute(sql)
+                    # Step3-1
+                    results = cursor.fetchall()
+                    return results
+                except MySQLdb.MySQLError as e:
+                    print(f"資料讀取錯誤: {e}")
+                    return None
